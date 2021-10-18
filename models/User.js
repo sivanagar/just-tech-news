7c618221d517c6697c1cc48f100a559350898e79
Sivan Kaplan <sivanagar@gmail.com>
2021-10-17 21:01:39 -0700

    password hash and login route

--- models/User.js
+++ models/User.js
@@ -1,8 +1,13 @@
+const bcrypt = require('bcrypt');
 const {Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 //creare our user model
-class User extends Model {};
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+};
 
 //define table columns and configuration
 User.init(
@@ -33,7 +38,16 @@ User.init(
         }
     },
     {
-        //TABLE CONFIG OPTIONS GO HERE
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password,10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamp: false,
         freezeTableName: true,
