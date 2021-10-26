82ea3b59ccb172311214ccf11ac519a03f5d60ac
Sivan Kaplan <sivanagar@gmail.com>
2021-10-25 20:13:57 -0700

    authentication

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,6 +6,7 @@ const {Post, User, Comment} = require('../models');
 
 //get all posts
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: [
             'id',
@@ -38,6 +39,15 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+      }
+    
+      res.render('login');
 });
 
 module.exports = router;
